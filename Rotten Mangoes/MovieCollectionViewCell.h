6195e3564759777321170e56696e443d2e3bd196
Alp Eren Can <alperenc@gmail.com>
2015-09-14 15:27:22 -0700

    implement custom cell and show movie posters

---
+++ Rotten Mangoes/MovieCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.h
+//  Rotten Mangoes
+//
+//  Created by Alp Eren Can on 14/09/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Rotten Mangoes/MovieCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.m
+//  Rotten Mangoes
+//
+//  Created by Alp Eren Can on 14/09/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import "MovieCollectionViewCell.h"
+
+@implementation MovieCollectionViewCell
+
+@end
