738c1ac96d1695642d5541a4a5d753fdca653a13
Alp Eren Can <alperenc@gmail.com>
2015-09-15 19:23:09 -0700

    add map to show nearby theaters and user location

--- Rotten Mangoes/AppDelegate.m
+++ Rotten Mangoes/AppDelegate.m
@@ -8,7 +8,7 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
+@interface AppDelegate () <CLLocationManagerDelegate>
 
 @end
 
@@ -16,6 +16,82 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     return YES;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    [self startLocationManager];
+}
+
+-(void)setUpLocationManager {
+    if (!_locationManager) {
+        _locationManager = [[CLLocationManager alloc] init];
+        _locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+        _locationManager.distanceFilter = 10;
+        //have to move 100m before location manager checks again
+        
+        _locationManager.delegate = self;
+        [_locationManager requestWhenInUseAuthorization];
+        NSLog(@"new location Manager in startLocationManager");
+        
+    }
+    
+    [_locationManager startUpdatingLocation];
+    NSLog(@"Start Regular Location Manager");
+}
+
+- (void)startLocationManager{
+    if ([CLLocationManager locationServicesEnabled]) {
+        
+        if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined){
+            [self setUpLocationManager];
+            
+        } else if (!([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied || [CLLocationManager authorizationStatus] == kCLAuthorizationStatusRestricted)){
+            [self setUpLocationManager];
+            
+        }else{
+            UIAlertController *alertController =[UIAlertController alertControllerWithTitle:@"Enable Location Services"
+                                                                              message:@"Location services are disabled, Please go into Settings > Privacy > Location to enable them for Play"
+                                                                       preferredStyle:UIAlertControllerStyleAlert];
+            [self.window.rootViewController presentViewController:alertController animated:YES completion:nil];
+            
+        }
+    }
+}
+
+-(void)stopLocationManager{
+    if ([CLLocationManager locationServicesEnabled]) {
+        if (_locationManager) {
+            [_locationManager stopUpdatingLocation];
+            NSLog(@"Stop Regular Location Manager");
+        }
+    }
+}
+
+-(void)locationManager:(nonnull CLLocationManager *)manager didUpdateLocations:(nonnull NSArray<CLLocation *> *)locations {
+    CLLocation * loc = [locations lastObject];
+    
+    NSLog(@"Time %@, latitude %+.6f, longitude %+.6f currentLocation accuracy %1.2f loc accuracy %1.2f timeinterval %f",[NSDate date],loc.coordinate.latitude, loc.coordinate.longitude, loc.horizontalAccuracy, loc.horizontalAccuracy, fabs([loc.timestamp timeIntervalSinceNow]));
+    
+    NSTimeInterval locationAge = -[loc.timestamp timeIntervalSinceNow];
+    if (locationAge > 10.0){
+        NSLog(@"locationAge is %1.2f",locationAge);
+        return;
+    }
+    
+    if (loc.horizontalAccuracy < 0){
+        NSLog(@"loc.horizontalAccuracy is %1.2f",loc.horizontalAccuracy);
+        return;
+    }
+    
+    if (_currentLocation == nil || _currentLocation.horizontalAccuracy >= loc.horizontalAccuracy){
+        _currentLocation = loc;
+        
+        if (loc.horizontalAccuracy <= _locationManager.desiredAccuracy) {
+            [self stopLocationManager];
+        }
+    }
 }
 
 @end
---
+++ Rotten Mangoes/MapViewController.m
@@ -0,0 +1,77 @@
+//
+//  MapViewController.m
+//  Rotten Mangoes
+//
+//  Created by Alp Eren Can on 15/09/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import "MapViewController.h"
+#import <MapKit/MapKit.h>
+#import "AppDelegate.h"
+
+#define zoominMapArea 2000
+
+@interface MapViewController () <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (nonatomic) CLLocation *currentLocation;
+
+@property (nonatomic) BOOL userLocationUpdated;
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.mapView.showsUserLocation = YES;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    self.mapView = nil;
+}
+
+- (void) initiateMap {
+    
+    AppDelegate *appDelegate = (AppDelegate *) [[UIApplication sharedApplication] delegate];
+    
+    self.currentLocation = [[CLLocation alloc] initWithLatitude:appDelegate.currentLocation.coordinate.latitude longitude:appDelegate.currentLocation.coordinate.longitude];
+    
+    CLLocationCoordinate2D zoomLocation = CLLocationCoordinate2DMake(self.currentLocation.coordinate.latitude, self.currentLocation.coordinate.longitude);
+    
+    MKCoordinateRegion adjustedRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, zoominMapArea, zoominMapArea);
+    
+    [self.mapView setRegion:adjustedRegion animated:YES];
+    
+}
+
+#pragma mark - MKMapView delegate -
+
+- (void)mapViewDidFinishLoadingMap:(nonnull MKMapView *)mapView{
+    [self initiateMap];
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
+    
+    if (!self.userLocationUpdated) {
+        [self.mapView setCenterCoordinate:self.mapView.userLocation.location.coordinate
+                                 animated:YES];
+        self.userLocationUpdated = YES;
+    }
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
